e8735e8c15f29eabbec12736e9830658efcb94d5
pauloernestom <pauloernestom@gmail.com>
2020-04-07 13:55:20 -0300

    login e logout

---
+++ routes/AdmRouter.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+const AdmController = require('../controllers/AdmController');
+
+router.get("/adm/login", AdmController.show);
+router.post('/adm/login', AdmController.login);
+router.get('/adm/logout', AdmController.logout);
+
+module.exports = router;
